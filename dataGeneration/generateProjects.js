f1c5227843bab6502a45cafbefb4596fc60fff5f
JayBo7 <jvboinapalli@ucdavis.edu>
2018-11-16 11:04:07 -0800

    create data for two tables and fix route for user pledging

---
+++ dataGeneration/generateProjects.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+const countries = ['CA', 'UK', 'US', 'AU', 'NZ', 'NL', 'DK', 'IE', 'NO', 'SE', 'DE',
+  'FR', 'ES', 'IT', 'AT', 'BE', 'CH', 'LU', 'HK', 'SG', 'MS', 'JP'];
+
+const records = 10 * 1000000;
+const stream = fs.createWriteStream('projects.csv');
+let i = 1;
+
+const write = () => {
+  while (i <= records) {
+    const project = {};
+    const randIdx = Math.floor(Math.random() * countries.length);
+
+    if (i % 5 === 0) {
+      project.location = countries[randIdx];
+    } else {
+      project.location = 'US';
+    }
+    stream.write(`${project.location}\n`);
+    if (i % 100000 === 0) {
+      console.log(`${i} entries logged`);
+    }
+    i += 1;
+  }
+};
+
+stream.on('drain', write);
+write();
